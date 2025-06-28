1ac7b1b29bbb8a550a14c3f9bc6f6932e0596621
Shreesh Lahauriya <slahauriya@deloitte.com>
2025-06-28 19:49:56 +0530

    added first Model (Url Model) class

---
+++ UrlShorty.Client/Data/Models/Url.cs
@@ -0,0 +1,30 @@
+﻿namespace UrlShorty.Client.Data.Models
+{
+	public class Url
+	{
+		// Unique identifier for the URL entity
+		public int Id { get; set; }
+
+		// The original, long URL to be shortened
+		public string? OriginalUrl { get; set; }
+
+		// The generated short code or token
+		public string? ShortenedUrl { get; set; }
+
+		// Date and time when the short URL was created
+		public DateTime? CreatedOn { get; set; }
+
+		// Date and time when the short URL was updated
+		public DateTime? UpdatedOn { get; set; }
+
+		// User or owner information, when authentication is used
+		public string? UserId { get; set; }
+
+		// Expiration date for the short URL
+		public DateTime? ExpirationDate { get; set; }
+
+		// Number of times the short URL has been accessed
+		public int ClickCount { get; set; } = 0;
+
+	}
+}
